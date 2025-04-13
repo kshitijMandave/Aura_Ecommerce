1475f8e24b86394a736e51bc3cc05227a9ef7b30
kshitijMandave <kshitij.mandwe@gmail.com>
2025-04-13 11:39:04 +0530

    Added more items in the server for better experience

--- ECommerce/src/Component/ProductsDetails/ProductDetails.jsx
+++ ECommerce/src/Component/ProductsDetails/ProductDetails.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 
 function ProductDetails() {
   let { id } = useParams();
-  // useParams retun one object, where all the parameter present in keyVal Pair {id: 10} so, we can destucture id from
+  // useParams retun one object, where all the parameter present in property (keyVal) Pair {id: 10} so, we can destucture id from
   return (
     <div>
       <h1>Product Details : {id}</h1>
